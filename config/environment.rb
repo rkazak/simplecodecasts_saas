1ac23c0b8e68948e859416c98137d3b402cb4688
Rohinton Kazak <rohinton.kazak@gmail.com>
2017-07-25 17:42:36 +0100

    Added in sendgrid configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# enable mail thur heroku's sendgrid.
+ActionMailer::Base.smtp_settings = {
+   :address => 'smtp.sendgrid.net',
+   :port => 587,
+   :authentication => :plain,
+   :user_name => ENV['SENDGRID_USERNAME'],
+   :password => ENV['SENDGRID_PASSWORD'],
+   :domain => 'herkou.com',
+   :enable_startstls_auto => true 
+}
+
